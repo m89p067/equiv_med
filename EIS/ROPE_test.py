b4919838be9e7a043000f55e6a5a2a25f8b5b233
m89p067 <92300319+m89p067@users.noreply.github.com>
2022-05-17 11:21:41 +0200

    Update ROPE_test.py

--- EIS/ROPE_test.py
+++ EIS/ROPE_test.py
@@ -110,6 +110,53 @@ class ROPE():
         print('Proportion of samples inside the ROPE Region :',round(rope_percentage*100,perc),' %')
         print('Equivalence based on ROPE :',out2)
         return rope_percentage
+    def rope_calc2(self):
+        # equal-tailed interval
+        my_data=self.x
+        my_range=self.rope_limits
+        ci=self.cred_int
+        CI_low=np.quantile(my_data,(1 - ci)/2) 
+        CI_high=np.quantile(my_data,(1 + ci)/2)
+        hdi_area=my_data[np.logical_and(my_data >=CI_low, my_data <= CI_high)]
+        area_within=hdi_area[np.logical_and(hdi_area >=min(my_range), hdi_area <= max(my_range) )]
+        rope_percentage = len(area_within)/len(hdi_area)
+        ROPE_low =my_range[0]
+        ROPE_high =my_range[1]
+  
+
+        return CI_low,CI_high,ROPE_low,ROPE_high,hdi_area,area_within
+    def rope_hdi2(self):
+        # highest density interval
+        my_data=self.x
+        my_range=self.rope_limits
+        ci=self.cred_int
+        x_sorted = np.sort(my_data, kind = "quicksort")
+        window_size = ceil(ci * len(x_sorted))
+        if window_size<2:
+            print('Warning : credibility interval or the dataset too small')
+        nCIs=len(x_sorted)-window_size
+        if nCIs<1:
+            print('Warning : credibility interval too large or the dataset too small')
+        for i in range(nCIs):
+            ci_width=x_sorted[i+window_size]-x_sorted[i]
+        min_i=np.where(ci_width==np.amin(ci_width)        )
+        n_minima = len(min_i)
+        if n_minima>1:
+            if any(np.diff(np.sort(min_i)) != 1):
+                print("Found identical values, selecting one...")
+                min_i=max(min_i)
+            else:
+                min_i=floor(mean(min_i))
+        CI_low = x_sorted[min_i[0]]        
+        CI_high = x_sorted[min_i[0] +    window_size]
+        hdi_area=my_data[np.logical_and(my_data >=CI_low, my_data <= CI_high)]
+        area_within=hdi_area[np.logical_and(hdi_area >=min(my_range), hdi_area <= max(my_range) )]
+        rope_percentage = len(area_within)/len(hdi_area)
+        ROPE_low =my_range[0]
+        ROPE_high =my_range[1]
+       
+
+        return CI_low,CI_high,ROPE_low,ROPE_high,hdi_area,area_within
     def plot_rope(self,which_rope='eti'):
         perc=1
         if which_rope=='eti':
@@ -128,3 +175,23 @@ class ROPE():
         plt.text(-1.2, -1.2, "Region of practical equivalence\nusing "+stringa, ha='left', va='center', fontsize=12)
         
         plt.show()
+        if len(self.x)>50 and ROPE.is_normal==True:
+            if which_rope=='eti':
+                lo,hi,r_lo,r_hi,area1,w_area1=self.rope_calc2()
+                stringa="equal-tailed interval"
+            else:
+                lo,hi,r_lo,r_hi,area1,w_area1=self.rope_hdi2()
+                stringa="highest density interval"
+            valori=self.x
+            x_val=np.linspace(valori.min(),valori.max(),num=len(self.x)*2,endpoint=True)
+            kernel = stats.gaussian_kde(valori)
+            fig1, ax1 = plt.subplots(figsize=(6, 6))
+            y_val=kernel(x_val)
+            ax1.plot(x_val,y_val,'k',label='Prob. Distr.')
+            ax1.hlines(y=min(y_val)+(min(y_val*0.1)), xmin=lo, xmax=hi, linewidth=3, color='r',label=stringa)
+            ax1.hlines(y=min(y_val)+(min(y_val*0.35)), xmin=r_lo, xmax=r_hi, linewidth=3, color='b',label='ROPE')
+            ax1.set_xlabel('Observed values')
+            ax1.set_ylabel('Probability Density Estimation')
+            plt.legend()
+            
+            plt.show()
