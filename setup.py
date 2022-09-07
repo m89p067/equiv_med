318e8a318458967ad8546cd2d65e371133423f92
m89p067 <92300319+m89p067@users.noreply.github.com>
2022-09-07 13:39:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 setup(
-    name='equiv-med',
+    name='equivmed',
     version='0.11',
     author='Mauro Nascimben',
     author_email='msnascimben@gmail.com',
     description='Functions for clinical equivalence testing',
-    url='https://github.com/m89p067/equiv-med',
-    packages=['equiv-med'],
+    url='https://github.com/m89p067/equivmed',
+    packages=['equivmed'],
     install_requires=['requests'],
 )
