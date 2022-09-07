bd0278350b2dbff101c7f25c447f29e2c8b0351c
m89p067 <92300319+m89p067@users.noreply.github.com>
2022-09-07 11:58:33 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='equiv_med',
+    version='0.11',
+    author='Mauro Nascimben',
+    author_email='msnascimben@gmail.com',
+    description='Functions for clinical equivalence testing',
+    url='https://github.com/m89p067/equiv_med',
+    packages=['equiv_med'],
+    install_requires=['requests'],
+)
