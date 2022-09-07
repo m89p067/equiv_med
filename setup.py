f1d10380b7edd52ee15368106ca6345823d05160
m89p067 <92300319+m89p067@users.noreply.github.com>
2022-09-07 14:01:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup, find_packages
+#__version__ = '0.11'
 requirements = [
-    "torch>=1.1.0",
-    "scipy",
-    "matplotlib",
-    "numpy>=1.17",
+    "scipy>=1.7.3",
+    "numpy>=1.22.1",
+    "matplotlib>=3.4.2",
+    "seaborn>=0.11.1",
+    "statsmodels>=0.12.2",
 ]
-
+test_requirements = ["pytest>=3"]
+#version = __version__
 setup(
     name='equivmed',
     version='0.11',
     author='Mauro Nascimben',
     author_email='msnascimben@gmail.com',
+    python_requires=">=3.6",
     description='Functions for clinical equivalence testing',
     url='https://github.com/m89p067/equivmed',
-    packages=['equivmed'],
-    install_requires=['requests'],
+    packages=find_packages(include=["equivmed", "equivmed.*"]),
+    install_requires=requirements,
+    #version=__version__,
 )
