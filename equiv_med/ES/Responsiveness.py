ec37a45f5f955a1fbc0fc1272b0bf4edb8f29d72
m89p067 <92300319+m89p067@users.noreply.github.com>
2022-09-09 10:39:03 +0200

    Update Responsiveness.py

--- equiv_med/ES/Responsiveness.py
+++ equiv_med/ES/Responsiveness.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.stats as stats
 from statistics import stdev
 from math import sqrt
-class Responsiveness():
+class Resp_indexes():
     is_normal=True
     same_var=True
     def __init__(self,control_t1,control_t2,treatment_t1,treatment_t2):
